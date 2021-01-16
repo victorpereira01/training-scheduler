051c19e9fac378b02b6d99d19ade2a80cbfb99ba
Victor Pereira <victor.cordeiro2@gmail.com>
2021-01-16 20:41:50 -0300

    feat: backend created

---
+++ backend/src/main/java/com/victorpereira/go4wod/Go4wodApplication.java
@@ -0,0 +1,13 @@
+package com.victorpereira.go4wod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Go4wodApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Go4wodApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/victorpereira/go4wod/Go4wodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victorpereira.go4wod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Go4wodApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
